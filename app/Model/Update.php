d0a404e535aa9c80e2df4e0ada8915de96316746
binbin4649 <binbin4649@gmail.com>
2014-08-13 12:51:25 +0900

    add update function

--- app/Model/Update.php
+++ app/Model/Update.php
@@ -4,6 +4,44 @@ App::uses('AppModel','Model');
 class Update extends AppModel {
     public $name = 'Update';
     public $useTable = 'updates';
-    // will set relative if needed after
-    //public $belongsTo = 'Book';
+
+    public $belongsTo = array(
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'user_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Book' => array(
+			'className' => 'Book',
+			'foreignKey' => 'book_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+
+    public function updateInfo($attrs){
+
+    	$this->create();
+    	if(!empty($attrs['book_id'])){
+    		$this->set('book_id',$attrs['book_id']);
+    		$book = $this->Book->find('first',array('conditions'=>array('Book.id'=>$attrs['book_id'])));	
+    	}
+
+        if(!empty($attrs['user_id'])) {
+        	$this->set('user_id',$attrs['user_id']);
+        }
+        
+	    if($attrs['event'] == 'bet_start'){
+	    	$content = '[Bet Now!] '.$book['Book']['title'].' | Bet start time:'.CakeTime::format( $book['Book']['bet_start'],'%Y/%m/%d %H:%M').' , The bet has started!';
+	    }elseif($attrs['event'] == 'bet_result'){
+	     	$content = '[Result Announcement!] '.$book['Book']['title'].' | Announcement time:'.CakeTime::format( $book['Book']['modified'],'%Y/%m/%d %H:%M').' ,  The results have been announced!';
+	    }
+	    $this->set('content',$content);
+	    $this->set('event',$attrs['event']);
+	    $this->save();
+	}
+
 }
